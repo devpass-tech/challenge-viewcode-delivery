1eb17dbee96c8dd4017a1d329ac743dc042b1fcc
sarafelix <x283609@prservicos.com.br>
2022-02-22 14:23:55 -0300

    more adjustment

--- solutions/devsprint-michael-douglas-2/DeliveryApp/Screens/Components/EmptyView.swift
+++ solutions/devsprint-michael-douglas-2/DeliveryApp/Screens/Components/EmptyView.swift
@@ -17,12 +17,14 @@ final class EmptyView: UIView {
         return emptyTitleTextLabel
     }()
     
-    private lazy var emptySubtitleTextField: UITextField = {
-        let emptySubtitleTextField = UITextField()
-        emptySubtitleTextField.translatesAutoresizingMaskIntoConstraints = false
-        emptySubtitleTextField.placeholder = "Procure por ruas com número e bairro utilizando o campo de busca"
-        emptySubtitleTextField.textAlignment = .center
-        return emptySubtitleTextField
+    private lazy var emptySubtitleLabel: UILabel = {
+        let emptySubtitleLabel = UILabel()
+        emptySubtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        emptySubtitleLabel.text = "Procure por ruas com número e bairro utilizando o campo de busca"
+        emptySubtitleLabel.textColor = .gray
+        emptySubtitleLabel.textAlignment = .center
+        emptySubtitleLabel.numberOfLines = 0
+        return emptySubtitleLabel
     }()
     
     init() {
@@ -38,16 +40,21 @@ final class EmptyView: UIView {
     
     func configureSubviews() {
         addSubview(emptyTitleLabel)
-        emptyTitleLabel.addSubview(emptySubtitleTextField)
+        emptyTitleLabel.addSubview(emptySubtitleLabel)
     }
     
     func configureConstraints() {
         NSLayoutConstraint.activate([
             emptyTitleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             emptyTitleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            emptyTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 120),
+            emptyTitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -120),
             
-            emptySubtitleTextField.centerXAnchor.constraint(equalTo: emptyTitleLabel.centerXAnchor, constant: 100),
-            emptySubtitleTextField.centerYAnchor.constraint(equalTo: emptyTitleLabel.centerYAnchor, constant: 60)
+            emptySubtitleLabel.centerXAnchor.constraint(equalTo: emptyTitleLabel.centerXAnchor, constant: 100),
+            emptySubtitleLabel.centerYAnchor.constraint(equalTo: emptyTitleLabel.centerYAnchor, constant: 60),
+            emptySubtitleLabel.leadingAnchor.constraint(equalTo: emptyTitleLabel.leadingAnchor),
+            emptySubtitleLabel.trailingAnchor.constraint(equalTo: emptyTitleLabel.trailingAnchor)
+            
         ])
     }
 }
