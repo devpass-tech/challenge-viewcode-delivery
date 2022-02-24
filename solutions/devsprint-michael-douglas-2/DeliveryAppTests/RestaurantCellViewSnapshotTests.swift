affdb904f975f6a3f42f0eeb23eb1b0868bd2e99
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-02-24 18:24:51 -0300

    Add new tests RestaurantCellView

--- solutions/devsprint-michael-douglas-2/DeliveryAppTests/RestaurantCellViewSnapshotTests.swift
+++ solutions/devsprint-michael-douglas-2/DeliveryAppTests/RestaurantCellViewSnapshotTests.swift
@@ -12,6 +12,7 @@ import SnapshotTesting
 class RestaurantCellViewSnapshotTests: XCTestCase {
     
     let size = CGSize(width: 300, height: 80)
+    let isRecord = false
 
     func test_restaurantCell_whenSnapshot_shouldSucceed() throws {
         let configuration = RestaurantCellViewConfiguration(name: "Percoriro Trattoria", detail: "Italiana • 38-48 min", icon: "restaurant-logo")
@@ -20,8 +21,47 @@ class RestaurantCellViewSnapshotTests: XCTestCase {
         restaurantCellView.backgroundColor = .white
         restaurantCellView.updateView(with: configuration)
         
-        assertSnapshot(matching: restaurantCellView, as: .image(size: size))
+        assertSnapshot(matching: restaurantCellView, as: .image(size: size), record: isRecord)
     }
-
+    
+    func test_restaurantCell_whenSnapshot_noName() throws {
+        let configuration = RestaurantCellViewConfiguration(name: "", detail: "Italiana • 38-48 min", icon: "restaurant-logo")
+        
+        let restaurantCellView = RestaurantCellView(style: .default, reuseIdentifier: nil)
+        restaurantCellView.backgroundColor = .white
+        restaurantCellView.updateView(with: configuration)
+        
+        assertSnapshot(matching: restaurantCellView, as: .image(size: size), record: isRecord)
+    }
+    
+    func test_restaurantCell_whenSnapshot_noDetail() throws {
+        let configuration = RestaurantCellViewConfiguration(name: "Percoriro Trattoria", detail: "", icon: "restaurant-logo")
+        
+        let restaurantCellView = RestaurantCellView(style: .default, reuseIdentifier: nil)
+        restaurantCellView.backgroundColor = .white
+        restaurantCellView.updateView(with: configuration)
+        
+        assertSnapshot(matching: restaurantCellView, as: .image(size: size), record: isRecord)
+    }
+    
+    func test_restaurantCell_whenSnapshot_noImage() throws {
+        let configuration = RestaurantCellViewConfiguration(name: "Percoriro Trattoria", detail: "Italiana • 38-48 min", icon: "")
+        
+        let restaurantCellView = RestaurantCellView(style: .default, reuseIdentifier: nil)
+        restaurantCellView.backgroundColor = .white
+        restaurantCellView.updateView(with: configuration)
+        
+        assertSnapshot(matching: restaurantCellView, as: .image(size: size), record: isRecord)
+    }
+    
+    func test_restaurantCell_whenSnapshot_largeName() throws {
+        let configuration = RestaurantCellViewConfiguration(name: "Percorirorororo Bar e Trattoria teste large name", detail: "Italiana • 38-48 min", icon: "restaurant-logo")
+        
+        let restaurantCellView = RestaurantCellView(style: .default, reuseIdentifier: nil)
+        restaurantCellView.backgroundColor = .white
+        restaurantCellView.updateView(with: configuration)
+        
+        assertSnapshot(matching: restaurantCellView, as: .image(size: size), record: isRecord)
+    }
 
 }
