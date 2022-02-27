6ae671e7555ccf60438fe98bbc86165568c38cb9
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-02-27 15:17:50 -0300

    create address view and update home view

---
+++ solutions/devsprint-michael-douglas-2/DeliveryApp/Screens/Components/AddressView.swift
@@ -0,0 +1,91 @@
+//
+//  AddressView.swift
+//  DeliveryApp
+//
+//  Created by Alexandre Cardoso on 21/02/22.
+//
+
+import UIKit
+
+protocol AddressViewDelegate: AnyObject {
+    func didEditAddress()
+}
+
+final class AddressView: UIView {
+    
+    private lazy var addressLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "R. Guiratinga, 500"
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor = .black
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var editButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setAttributedTitle(
+            NSAttributedString(
+                string: "Editar",
+                attributes: [.font: UIFont.systemFont(ofSize: 15, weight: .bold)]
+            ),
+            for: .normal
+        )
+        button.layer.cornerRadius = 10
+        button.layer.borderColor = UIColor.link.cgColor
+        button.layer.borderWidth = 1.0
+        button.addTarget(self, action: #selector(editAddress(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    weak var delegate: AddressViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateView(address: String) {
+        addressLabel.text = address
+    }
+    
+    private func setupView() {
+        buildHierarchy()
+        buildConstraints()
+    }
+    
+    @objc
+    private func editAddress(_ sender: UIButton) {
+        delegate?.didEditAddress()
+    }
+    
+}
+
+extension AddressView {
+
+    private func buildHierarchy() {
+        addSubview(addressLabel)
+        addSubview(editButton)
+    }
+    
+    private func buildConstraints() {
+        NSLayoutConstraint.activate([
+            addressLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            addressLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            editButton.heightAnchor.constraint(equalToConstant: 40),
+            editButton.widthAnchor.constraint(equalToConstant: 70),
+            editButton.leadingAnchor.constraint(equalTo: addressLabel.trailingAnchor, constant: 16),
+            editButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+    }
+    
+}
