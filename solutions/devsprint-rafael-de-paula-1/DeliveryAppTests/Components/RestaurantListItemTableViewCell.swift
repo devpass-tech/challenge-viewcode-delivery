a62b1478ceebaa49e6778d75ccae75894cf3bb81
Rafael de Paula <rafsouzap@icloud.com>
2022-04-09 09:51:38 -0300

    Refactoring restaurant list cell

--- solutions/devsprint-rafael-de-paula-1/DeliveryAppTests/Components/RestaurantListItemTableViewCell.swift
+++ solutions/devsprint-rafael-de-paula-1/DeliveryAppTests/Components/RestaurantListItemTableViewCell.swift
@@ -5,19 +5,30 @@
 //  Created by Thyago Raphael on 07/04/22.
 //
 
-import XCTest
 import SnapshotTesting
+import XCTest
 
 @testable import DeliveryApp
 
-class RestauranteCellViewSnapshotTests: XCTestCase {
+final class RestauranteCellViewSnapshotTests: XCTestCase {
     
-    let size = CGSize(width: 300, height: 80)
-    let isRecord = true
+    private let size = CGSize(width: 390, height: 80)
+    private let isRecord = false
     
-    func test_tableViewCell_shouldSucceed() throws {
-        let sut = RestaurantListItemTableViewCell()
+    private lazy var sut = RestaurantListItemTableViewCell()
+    
+    func test_whenRestaurantNameIsShort() throws {
+        sut.configure(with: .init(name: "Abbraccio", category: "Italiana", minDeliveryTime: 65, maxDeliveryTime: 75, icon: "restaurat-logo"))
         assertSnapshot(matching: sut, as: .image(size: size), record: isRecord)
     }
     
+    func test_whenRestaurantNameIsLong() throws {
+        sut.configure(with: .init(name: "Churrascaria Costelão Gaúcho da Serra", category: "Carnes", minDeliveryTime: 48, maxDeliveryTime: 65, icon: "restaurat-logo"))
+        assertSnapshot(matching: sut, as: .image(size: size), record: isRecord)
+    }
+    
+    func test_whenRestaurantIconIsNil() throws {
+        sut.configure(with: .init(name: "Bar do Alemão", category: "Brasileira", minDeliveryTime: 42, maxDeliveryTime: 52, icon: nil))
+        assertSnapshot(matching: sut, as: .image(size: size), record: isRecord)
+    }
 }
