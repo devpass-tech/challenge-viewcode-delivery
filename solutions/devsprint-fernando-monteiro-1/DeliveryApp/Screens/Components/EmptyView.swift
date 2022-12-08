21f9cb99e493d63b730270997bd23f2d91bf69de
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-12-08 01:02:19 -0300

    Adicionando EmptyView e seu preview.
    Adicionando componentes da EmptyView e adicionando live preview para essa view.

--- solutions/devsprint-fernando-monteiro-1/DeliveryApp/Screens/Components/EmptyView.swift
+++ solutions/devsprint-fernando-monteiro-1/DeliveryApp/Screens/Components/EmptyView.swift
@@ -8,5 +8,81 @@
 import UIKit
 
 class EmptyView: UIView {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Endereço não encontrado"
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Procure por ruas com número e bairro utilizando o campo de busca"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 15
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+
+        self.setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
+
+private extension EmptyView {
+
+    func setupViews() {
+
+        self.configureSubviews()
+        self.configureSubviewsConstraints()
+    }
+
+    func configureSubviews() {
+        self.addSubview(stackView)
+    }
+
+    func configureSubviewsConstraints() {
+
+        NSLayoutConstraint.activate([
+            subtitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 53),
+            subtitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -54),
+            
+            stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+        ])
+    }
+}
+
+#if DEBUG
+import SwiftUI
+
+struct EmptyView_Preview: PreviewProvider {
+    static var previews: some View {
+        let emptyView = EmptyView()
+        
+        return emptyView.showPreview()
+    }
+}
+#endif
