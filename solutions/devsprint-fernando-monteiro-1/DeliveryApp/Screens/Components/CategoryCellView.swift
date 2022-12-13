b75de484eb36957b313a0bd9c8ad27725b1efd90
Christian Rezende <christian.rezende@vortigo.com.br>
2022-12-12 22:57:32 -0300

    chore: add category cell view and snapshot

--- solutions/devsprint-fernando-monteiro-1/DeliveryApp/Screens/Components/CategoryCellView.swift
+++ solutions/devsprint-fernando-monteiro-1/DeliveryApp/Screens/Components/CategoryCellView.swift
@@ -7,6 +7,90 @@
 
 import UIKit
 
-class CategoryCellView: UIView {
+class CategoryCellView: UITableViewCell {
+    
+    private let stackview:UIStackView = {
+        let view = UIStackView(frame: CGRect.zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.alignment = .center
+        view.spacing = 12
+        return view
+    }()
+    
+    private let image:UIImageView = {
+        let view = UIImageView(frame: CGRect.zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 14
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private let label:UILabel = {
+        let label = UILabel(frame: CGRect.zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 11)
+        label.isOpaque = true
+        return label
+    }()
 
+    init(style: UITableViewCell.CellStyle, reuseIdentifier: String?,imageName:String,text:String) {
+
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setupViews(imageName: imageName, text: text)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews(imageName:String,text:String){
+        self.image.image = UIImage(named: imageName)
+        self.label.text = text
+     
+        configureSubviews()
+        
+        configureSubviewsConstraints()
+        
+    }
+    
+    func configureSubviews() {
+        self.contentView.addSubview(stackview)
+        
+        self.stackview.addArrangedSubview(image)
+        self.stackview.addArrangedSubview(label)
+    }
+
+    func configureSubviewsConstraints() {
+
+        NSLayoutConstraint.activate([
+            image.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            image.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            image.widthAnchor.constraint(equalToConstant: 50),
+            image.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
 }
+
+
+#if DEBUG
+import SwiftUI
+
+struct CategoryCellView_Preview: PreviewProvider {
+    static var previews: some View {
+        let emptyView = CategoryCellView.stub()
+        
+        return emptyView.showPreview()
+    }
+}
+
+extension CategoryCellView {
+    
+    static func stub() -> CategoryCellView {
+        let imageName = "pizza.jpeg"
+        let text = "Mercado"
+        return CategoryCellView(style: .default, reuseIdentifier: "test",imageName: imageName, text: text)
+    }
+}
+#endif
--- solutions/devsprint-fernando-monteiro-1/DeliveryAppTests/CategoryCellViewSnapshotTests.swift
+++ solutions/devsprint-fernando-monteiro-1/DeliveryAppTests/CategoryCellViewSnapshotTests.swift
@@ -6,7 +6,18 @@
 //
 
 import XCTest
+import SnapshotTesting
+@testable import DeliveryApp
 
-class CategoryCellViewSnapshotTests {
-
+class CategoryCellViewSnapshotTests: XCTestCase {
+    func test_categorycell_view_defaultState() {
+        let emtpyView = CategoryCellView.stub()
+        emtpyView.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+        
+        let result = verifySnapshot(matching: emtpyView,
+                                    as: .image,
+                                    named: "Default",
+                                    testName: "CategoryCellView")
+        XCTAssertNil(result)
+    }
 }
